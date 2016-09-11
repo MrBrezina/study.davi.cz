f1100cfb063b80013b433b5bffde027d213cf132
David Březina <brezina@davi.cz>
2016-09-11 22:22:05 +0100

    Added test form

---
+++ test/triplets-1.js
@@ -0,0 +1,9 @@
+// sequences of triplets
+
+var sequences = [
+// PT Serif Latin: triplet combinations of ['h', 'n', 'm', 'z', 'g', 'q', 'r', 'y']
+['pt-serif_latin', [['h', 'm', 'n']]]
+,
+// PT Sans Latin: triplet combinations of ['q', 'p', 'b', 'z', 'w', 'f', 'n', 'i']
+['pt-sans_latin', [['b', 'p', 'q']]]
+];
