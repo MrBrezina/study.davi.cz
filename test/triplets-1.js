96ccd2a9b8857fe42f9d3a7671c5d5ecfbb5a2fa
David Březina <brezina@davi.cz>
2016-09-24 19:56:55 +0100

    Transferred everything to GetForm and removed test form

--- test/triplets-1.js
+++
@@ -1,9 +0,0 @@
-// sequences of triplets
-
-var sequences = [
-// PT Serif Latin: triplet combinations of ['h', 'n', 'm', 'z', 'g', 'q', 'r', 'y']
-['pt-serif_latin', [['h', 'm', 'n']]]
-,
-// PT Sans Latin: triplet combinations of ['q', 'p', 'b', 'z', 'w', 'f', 'n', 'i']
-['pt-sans_latin', [['b', 'p', 'q']]]
-];
